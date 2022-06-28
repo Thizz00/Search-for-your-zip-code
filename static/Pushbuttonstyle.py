f3471f391ac4a2fab9754810fa931dbe62296376
Thizz00 <97180137+Thizz00@users.noreply.github.com>
2022-06-28 16:23:55 +0200

    Add files via upload

---
+++ static/Pushbuttonstyle.py
@@ -0,0 +1,13 @@
+class Stylesheetpushbutton(object):
+    def Pushbuttonstyles(self):
+        return("QPushButton{\n"
+"color:rgba(255,255,255,210);\n"
+"background-color:rgba(0,0,0,0);\n"
+"border-radius:20px;\n"
+"}\n"
+"QPushButton:hover{\n"
+"color:#F9F956;\n"
+"}\n"
+"QPushButton:pressed{\n"
+"color:rgb(111,81,118);\n"
+"}")
